beab7998b7ad3376411fab8214e2ecdf4cb92964
One1turn <2627079579@qq.com>
2026-08-19 12:34:25 +0800

    Add example.py

---
+++ example.py
@@ -0,0 +1,26 @@
+# CodeRoast 示例：一段该被烤的代码
+import os
+
+def do_stuff(a, b, c):
+    x = 100
+    y = 200
+    tmp = a + b + c * x + y * 999
+    if tmp > 0:
+        if tmp > 100:
+            if tmp > 1000:
+                if tmp > 10000:
+                    if tmp > 100000:
+                        print("deep")
+    return tmp
+
+password = "admin123"
+api_key = "sk-secret-12345"
+
+data = eval(input())
+result = data
+result = data
+result = data
+
+# TODO: fix this later
+# FIXME: this is broken
+pass
